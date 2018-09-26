7a35ca74de89e5e47db26e9a51cbdc5b68ffa10a
ice48623 <ice48623@gmail.com>
2018-09-26 16:20:23 +0700

    update readme, resize gif to 320h

--- thumbnail-generator.py
+++ thumbnail-generator.py
@@ -10,8 +10,8 @@ def main():
     file_format = "frame-%03d.png"
 
     call(["mkdir", BASE_FOLDER + folder_name])
-    call(["ffmpeg", "-i", BASE_FOLDER + input_file, "-r", "1", BASE_FOLDER + folder_name + "/" + file_format])
-    call(["convert", "-delay", "100", "-loop", "0", BASE_FOLDER + folder_name + "/*.png", BASE_FOLDER + output_file])
+    call(["ffmpeg", "-i", BASE_FOLDER + input_file, "-r", "1", "-vf", "scale=320:-1", BASE_FOLDER + folder_name + "/" + file_format])
+    call(["convert", "-delay", "15", "-loop", "0", BASE_FOLDER + folder_name + "/*.png", BASE_FOLDER + output_file])
 
 if __name__ == '__main__':
     main()
