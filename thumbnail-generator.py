e3d1c5d4c2f89f1bc9e01d844d25679ddf791332
ice48623 <ice48623@gmail.com>
2018-09-26 16:01:35 +0700

    working

---
+++ thumbnail-generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from subprocess import call
+import os
+
+def main():
+    input_file = os.environ['input']
+    output_file = os.environ['output']
+    BASE_FOLDER = "./video/"
+    folder_name = os.environ['input'] + "-frames"
+    file_format = "frame-%03d.png"
+
+    call(["mkdir", BASE_FOLDER + folder_name])
+    call(["ffmpeg", "-i", BASE_FOLDER + input_file, "-r", "1", BASE_FOLDER + folder_name + "/" + file_format])
+    call(["convert", "-delay", "100", "-loop", "0", BASE_FOLDER + folder_name + "/*.png", BASE_FOLDER + output_file])
+
+if __name__ == '__main__':
+    main()
