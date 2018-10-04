37f3c5be2331058a7904d3ec45ac8d0ef3e2d48a
ice48623 <ice48623@gmail.com>
2018-10-04 20:26:51 +0700

    upload to sos

--- thumbnail-generator.py
+++ thumbnail-generator.py
@@ -8,7 +8,7 @@ import requests
 import pika
 import time
 import os
-
+import hashlib
 
 def get_video_info(pathToInputVideo):
     cmd = "ffprobe -v quiet -print_format json -show_streams"
@@ -39,16 +39,43 @@ def download_file(video_host, video_bucket_name, video_object_name, port = "8080
         os.makedirs(directory)
 
     r = requests.get(url, stream=True)
-    total_length = int(r.headers.get('Content-Length'))
-    collected = 0
     with open(save_location, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
-                collected += len(chunk)
                 #f.flush() commented by recommendation from J.F.Sebastian
-    return total_length == collected
+    return save_location
 
+def md5(file_path):
+    hash_md5 = hashlib.md5()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+def create_upload_ticket(target_host, target_bucket_name, target_object_name, port="8080"):
+    url = "http://" + target_host + ":" + port + "/" + target_bucket_name + "/" + target_object_name + "?create"
+    requests.post(url)
+
+def complete_upload(target_host, target_bucket_name, target_object_name, port="8080"):
+    url = "http://" + target_host + ":" + port + "/" + target_bucket_name + "/" + target_object_name + "?complete"
+    requests.post(url)
+
+def upload_file(file_path, target_host, target_bucket_name, target_object_name, part_number="1", port = "8080"):
+    print("uploading")
+    file_size = 0
+    try:
+        file_size = os.path.getsize(file_path)
+    except:
+        print("file not found")
+    create_upload_ticket(target_host, target_bucket_name, target_object_name)
+    with open(file_path,'rb') as payload:
+        headers = {'Content-Type': 'application/octet-stream', 'Content-Length': str(file_size), 'Content-MD5': str(md5(file_path))}
+        url = "http://" + target_host + ":" + port + "/" + target_bucket_name + "/" + target_object_name + "?partNumber=" + part_number
+        print(url)
+        requests.put(url, data=payload, headers=headers)
+
+    complete_upload(target_host, target_bucket_name, target_object_name)
 def generateGIF(video_host, video_bucket_name, video_object_name, target_host, target_bucket_name, target_object_name):
     print("Generating GIF")
     file_format = "frame-%03d.png"
@@ -65,10 +92,14 @@ def generateGIF(video_host, video_bucket_name, video_object_name, target_host, t
     output_fps = extracted_frame / output_duration
     interval = round(60 / output_fps, 1) * 10
 
+    output_file_path = BASE_FOLDER + target_bucket_name + "/" + target_object_name
+
     call(["mkdir", BASE_FOLDER + video_bucket_name + "/frames"])
     call(["ffmpeg", "-i", BASE_FOLDER + video_bucket_name + "/" + video_object_name, "-r", str(desired_frame), "-vf", "scale=320:-1", BASE_FOLDER + video_bucket_name + "/frames/" + file_format])
     call(["convert", "-delay", str(interval), "-loop", "0", BASE_FOLDER + video_bucket_name + "/frames/*.png", BASE_FOLDER + target_bucket_name + "/" + target_object_name])
     call(["rm", "-rf", BASE_FOLDER + video_bucket_name + "/frames"])
+    print("GIF generated!")
+    return output_file_path
 
 def parser(ch, method, properties, body):
     print(" [x] Received %r" % body)
@@ -85,7 +116,8 @@ def parser(ch, method, properties, body):
 
 def logic(video_host, video_bucket_name, video_object_name, target_host, target_bucket_name, target_object_name):
     download_file(video_host, video_bucket_name, video_object_name)
-    generateGIF(video_host, video_bucket_name, video_object_name, target_host, target_bucket_name, target_object_name)
+    output_file_path = generateGIF(video_host, video_bucket_name, video_object_name, target_host, target_bucket_name, target_object_name)
+    upload_file(output_file_path, target_host, target_bucket_name, target_object_name)
 
 print("Start worker")
 time.sleep(5)
